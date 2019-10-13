a88d0dcd4b10c70333a31362d753a6599003955e
Camille Bouzerand <camille.bouzerand@epitech.eu>
2019-10-13 15:22:05 +0200

    fix(audio.cpp/interface_client): add doc

--- audio/srcs/PortAudio.cpp
+++ audio/srcs/PortAudio.cpp
@@ -7,6 +7,13 @@
 
 #include "PortAudio.hpp"
 
+/*!
+ * \fn PortAudio::PortAudio()
+ * \brief ?
+ *
+ * \param void
+ */
+
 PortAudio::PortAudio():
     _sample_rate(44100),
     _frame_per_buffer(512)
@@ -16,12 +23,26 @@ PortAudio::PortAudio():
         exit(84);
 }
 
+/*!
+ * \fn PortAudio::~PortAudio()
+ * \brief ?
+ *
+ * \param void
+ */
+
 PortAudio::~PortAudio()
 {
     Pa_Terminate();
     if (_data.recordedSamples)
         free(_data.recordedSamples);
 }
+
+/*!
+ * \fn PortAudio::SetInputParameters()
+ * \brief ?
+ *
+ * \param void
+ */
 
 void PortAudio::SetInputParameters()
 {
@@ -35,6 +56,13 @@ void PortAudio::SetInputParameters()
     inputParameters.suggestedLatency = Pa_GetDeviceInfo(inputParameters.device)->defaultLowInputLatency;
     inputParameters.hostApiSpecificStreamInfo = NULL;
 }
+
+/*!
+ * \fn PortAudio::SetData()
+ * \brief ?
+ *
+ * \param int num_seconds, int sample_rate, int num_channels
+ */
 
 void PortAudio::SetData(int num_seconds, int sample_rate, int num_channels)
 {
@@ -51,6 +79,13 @@ void PortAudio::SetData(int num_seconds, int sample_rate, int num_channels)
         _data.recordedSamples[i] = 0;
 }
 
+/*!
+ * \fn PortAudio::SetOutputParameters()
+ * \brief ?
+ *
+ * \param void
+ */
+
 void PortAudio::SetOutputParameters()
 {
     outputParameters.device = Pa_GetDefaultInputDevice();
@@ -64,6 +99,13 @@ void PortAudio::SetOutputParameters()
     outputParameters.hostApiSpecificStreamInfo = NULL;
 }
 
+/*!
+ * \fn PortAudio::StartStream()
+ * \brief ?
+ *
+ * \param PaStream *stream
+ */
+
 void PortAudio::StartStream(PaStream *stream)
 {
     err = Pa_StartStream(stream);
@@ -71,12 +113,31 @@ void PortAudio::StartStream(PaStream *stream)
         exit(84);
 }
 
+/*!
+ * \fn PortAudio::CloseStream()
+ * \brief ?
+ *
+ * \param PaStream *stream
+ */
+
 void PortAudio::CloseStream(PaStream *stream)
 {
     err = Pa_CloseStream(stream);
     if (err != paNoError)
         exit(84);
 }
+
+/*!
+ * \fn recordCallback()
+ * \brief ?
+ *
+ * \param const void *inputBuffer,
+                    void *outputBuffer,
+                    unsigned long framesPerBuffer,
+                    const PaStreamCallbackTimeInfo *timeInfo,
+                    PaStreamCallbackFlags statusFlags,
+                    void *userData
+ */
 
 int recordCallback(const void *inputBuffer,
                     void *outputBuffer,
@@ -116,6 +177,13 @@ int recordCallback(const void *inputBuffer,
     return finished;
 }
 
+/*!
+ * \fn PortAudio::RecordStream()
+ * \brief ?
+ *
+ * \param void
+ */
+
 PaStream *PortAudio::RecordStream()
 {
     PaStream *stream;
@@ -136,6 +204,17 @@ PaStream *PortAudio::RecordStream()
     }
     return stream;
 }
+
+/*!
+ * \fn playCallback()
+ * \brief ?
+ *
+ * \param const void *inputBuffer, void *outputBuffer,
+                        unsigned long framesPerBuffer,
+                        const PaStreamCallbackTimeInfo *timeInfo,
+                        PaStreamCallbackFlags statusFlags,
+                        void *userData
+ */
 
 static int playCallback(const void *inputBuffer, void *outputBuffer,
                         unsigned long framesPerBuffer,
@@ -173,6 +252,13 @@ static int playCallback(const void *inputBuffer, void *outputBuffer,
     return finished;
 }
 
+/*!
+ * \fn PortAudio::PlayStream()
+ * \brief ?
+ *
+ * \param PaStream *stream
+ */
+
 void PortAudio::PlayStream(PaStream *stream)
 {
     err = Pa_OpenStream(
@@ -191,16 +277,36 @@ void PortAudio::PlayStream(PaStream *stream)
     }
 }
 
+/*!
+ * \fn PortAudio::setSampleRate()
+ * \brief ?
+ *
+ * \param short sample_rate
+ */
 
 void PortAudio::setSampleRate(short sample_rate)
 {
     _sample_rate = sample_rate;
 }
 
+/*!
+ * \fn PortAudio::setFramePerBuffer()
+ * \brief ?
+ *
+ * \param short frame_per_buffer
+ */
+
 void PortAudio::setFramePerBuffer(short frame_per_buffer)
 {
     _frame_per_buffer = frame_per_buffer;
 }
+
+/*!
+ * \fn PortAudio::setDataFrameIndex()
+ * \brief ?
+ *
+ * \param void
+ */
 
 void PortAudio::setDataFrameIndex()
 {
