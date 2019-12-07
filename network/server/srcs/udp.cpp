7cc42185a0ae83a7debe101acb8655e017cf7b3a
Camille Bouzerand <camille.bouzerand@epitech.eu>
2019-12-07 23:30:55 +0100

    merge between remastered and boost server work

---
+++ network/server/srcs/udp.cpp
@@ -0,0 +1,177 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/asio.hpp>
+#include <list>
+#include <thread>
+#include <mutex>
+#include <ctime>
+
+using boost::asio::ip::udp;
+
+enum messageType {
+    WELCOME = 0,
+    INFO = 1,
+    UNKNOW = 2
+};
+
+std::string make_daytime_string()
+{
+    using namespace std; // For time_t, time and ctime;
+    time_t now = time(0);
+    return ctime(&now);
+}
+
+class udp_server
+{
+    public:
+        udp_server(boost::asio::io_context& io_context, short port)
+            : _socket(io_context, udp::endpoint(udp::v4(), port)),
+            _clientNb(1)
+        {
+            _thread_check = std::thread(&udp_server::check_clock, this, std::ref(_list_client));
+            start_receive();
+        }
+
+    private:
+
+        void check_clock(std::list<std::tuple<udp::endpoint, int, std::clock_t> >& _list)
+        {
+            while (1) {
+                usleep(1000000); //modifier lui pour plus de précisions
+                for (auto const& i : _list) {
+                    std::clock_t c_end = std::clock();
+                    // std::cout << std::get<2>(i) << " and " << c_end << std::endl;
+                    int result = (c_end-std::get<2>(i)) / 100;
+                    // std::cout << "le temps: " << result << std::endl;
+                    if (result >= 10) {
+                        std::cout << "deco du client" << std::get<1>(i) << std::endl;
+                        _list.remove(std::make_tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i)));
+                        break;
+                    }
+                }
+            }
+        }
+
+        void start_receive()
+        {
+            _socket.async_receive_from(
+                boost::asio::buffer(_recv_buffer), _remote_endpoint,
+                boost::bind(&udp_server::handle_receive, this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+        }
+
+        void handle_receive(const boost::system::error_code& error,
+            std::size_t buff_size)
+        {
+            if (!error) {
+                boost::shared_ptr<std::string> message(
+                new std::string(make_daytime_string()));
+
+                bool isExist = false;
+                std::string data(_recv_buffer.begin(), buff_size);
+                std::cout << "Message de " << _remote_endpoint << ": " << data << std::endl;
+                for (auto const& i : _list_client) {
+                    if (std::get<0>(i) == _remote_endpoint) {
+                        std::cout << "il existe deja" << std::endl;
+                        *(std::clock_t *)&std::get<2>(i) = std::clock();
+                        find_command(data, buff_size, std::get<1>(i));
+                        isExist = true;
+                        break;
+                    }
+                }
+                if (isExist == false) {
+                    std::clock_t c_start = std::clock();
+                    _list_client.push_back(std::make_tuple(_remote_endpoint, _clientNb, c_start));
+                    sender_message(WELCOME, _clientNb);
+                    _clientNb++;
+                    std::cout << "il existe pas" << std::endl;
+                    start_receive();
+                }
+                start_receive();
+            }
+        }
+
+        void handle_send(boost::shared_ptr<std::string> /*message*/,
+            const boost::system::error_code& /*error*/,
+            std::size_t /*bytes_transferred*/)
+        {
+        }
+
+        void find_command(std::string data, std::size_t buff_size, int currentClient)
+        {
+            if (data.compare("call") == 0)
+                call_fct();
+            else if (data.compare("info") == 0)
+                sender_message(INFO, currentClient);
+            else
+                sender_message(UNKNOW, currentClient);
+        }
+
+        void sender_message(messageType message_type, int currentClient)
+        {
+            std::string w_message;
+            if (message_type == WELCOME)
+                w_message += "Welcome, you're the client " + std::to_string(currentClient) + "\n";
+            else if (message_type == INFO)
+                w_message += "As you have asked client " + std::to_string(currentClient) + "\n";
+            else if (message_type == UNKNOW)
+                w_message += "Your command is unknow.\n";
+
+            if (message_type == WELCOME || message_type == INFO) {
+                w_message += "Here you can see the list of client connected:\n";
+                for (auto const& i : _list_client) {
+                    std::stringstream ss;
+                    ss << std::get<0>(i);
+                    std::string str = ss.str();
+                    w_message += "-client " + std::to_string(std::get<1>(i)) + " on " + str + "\n";
+                }
+            }
+
+            if (message_type == INFO || message_type == UNKNOW)
+                w_message += "Here you can see the list of commands:\n-info = get information about clients connected\n-call \"client nb\" = call the client depends on the number given\n";
+
+            boost::shared_ptr<std::string> message(new std::string(w_message));
+            _socket.async_send_to(boost::asio::buffer(*message), _remote_endpoint,
+                boost::bind(&udp_server::handle_send, this, message,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+        }
+
+        void call_fct()
+        {
+            std::string w_message("Ouais ca arrive la feature\n");
+            boost::shared_ptr<std::string> message(new std::string(w_message));
+            _socket.async_send_to(boost::asio::buffer(*message), _remote_endpoint,
+                boost::bind(&udp_server::handle_send, this, message,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+        }
+
+        udp::socket _socket;
+        udp::endpoint _remote_endpoint;
+        boost::array<char, 1024> _recv_buffer;
+        std::list<std::tuple<udp::endpoint, int, std::clock_t> > _list_client;
+        std::thread _thread_check;
+        int _clientNb;
+};
+
+int main(int argc, char **argv)
+{
+  try
+  {
+    boost::asio::io_context io_context;
+    udp_server server(io_context, std::atoi(argv[1]));
+    io_context.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
---
+++ nike.cpp
@@ -0,0 +1,92 @@
+// #include <iostream>
+// #include <boost/array.hpp>
+// #include <boost/asio.hpp>
+
+// using boost::asio::ip::udp;
+
+// class UDPClient
+// {
+// public:
+//     UDPClient(
+//         boost::asio::io_service& io_service, 
+//         const std::string& host, 
+//         const std::string& port
+//     ) : io_service_(io_service), socket_(io_service, udp::endpoint(udp::v4(), 0)) {
+//         udp::resolver resolver(io_service_);
+//         udp::resolver::query query(udp::v4(), host, port);
+//         udp::resolver::iterator iter = resolver.resolve(query);
+//         endpoint_ = *iter;
+//     }
+
+//     ~UDPClient()
+//     {
+//         socket_.close();
+//     }
+
+//     void send(const std::string& msg) {
+//         socket_.send_to(boost::asio::buffer(msg, msg.size()), endpoint_);
+//     }
+
+//     // void receive()
+//     // {
+//     //     while (1) {
+//     //         socket_.receive_from
+//     //     }
+//     // }
+
+// private:
+//     boost::asio::io_service& io_service_;
+//     udp::socket socket_;
+//     udp::endpoint endpoint_;
+// };
+
+// int main(int argc, char **argv)
+// {
+//     boost::asio::io_service io_service;
+//     UDPClient client(io_service, "localhost", argv[1]);
+
+//     while (1) {
+//         std::string str;
+//         std::getline(std::cin, str);
+//         client.send(str);
+//     }
+// }
+
+
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::udp;
+
+int main()
+{
+    try
+    {
+        boost::asio::io_service io_service;
+
+        udp::endpoint receiver_endpoint (boost::asio::ip::address::from_string("127.0.0.1"), 4242);
+        
+        udp::socket socket(io_service); // (1)
+        socket.open(udp::v4());
+
+        while (1) {
+            // boost::array<char, 6> send_buf  = { 'H', 'e', 'l', 'l', 'o' }; // (2)
+            std::string str;
+            std::getline(std::cin, str);
+            socket.send_to(boost::asio::buffer(str), receiver_endpoint); // (3)
+
+            boost::array<char, 1024> recv_buf; // (4)
+            udp::endpoint sender_endpoint;
+            size_t len = socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint); // (5)
+
+            std::cout.write(recv_buf.data(), len); // (6)
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
