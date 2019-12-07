0d8a98f9d40d7162ed7a9790c4e175867510e0de
Camille Bouzerand <camille.bouzerand@epitech.eu>
2019-12-07 12:16:56 +0100

    add(clock): add a clock on the list for each client

--- network/server/srcs/udp.cpp
+++ network/server/srcs/udp.cpp
@@ -8,6 +8,7 @@
 #include <list>
 #include <thread>
 #include <mutex>
+#include <ctime>
 
 using boost::asio::ip::udp;
 
@@ -25,61 +26,84 @@ class udp_server
             : _socket(io_context, udp::endpoint(udp::v4(), port)),
             _clientNb(1)
         {
+            _thread_check = std::thread(&udp_server::check_clock, this, std::ref(_list_client));
             start_receive();
         }
 
     private:
-    void start_receive()
-    {
-        _socket.async_receive_from(
-            boost::asio::buffer(_recv_buffer), _remote_endpoint,
-            boost::bind(&udp_server::handle_receive, this,
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
-    }
 
-    void handle_receive(const boost::system::error_code& error,
-        std::size_t buff_size)
-    {
-        if (!error) {
-            boost::shared_ptr<std::string> message(
-            new std::string(make_daytime_string()));
-
-            bool isExist = false;
-            std::string data(_recv_buffer.begin(), buff_size);
-            std::cout << "Message de " << _remote_endpoint << ": " << data << std::endl;
-            for (auto const& i : _list_client) {
-                if (i.first == _remote_endpoint) {
-                    std::cout << "il existe deja" << std::endl;
-                    isExist = true;
-                    break;
+        void check_clock(std::list<std::tuple<udp::endpoint, int, std::clock_t> >& _list)
+        {
+            while (1) {
+                usleep(1000000); //modifier lui pour plus de précisions
+                for (auto const& i : _list) {
+                    std::clock_t c_end = std::clock();
+                    // std::cout << std::get<2>(i) << " and " << c_end << std::endl;
+                    int result = (c_end-std::get<2>(i)) / 100;
+                    // std::cout << "le temps: " << result << std::endl;
+                    if (result >= 5) {
+                        std::cout << "deco du client" << std::get<1>(i) << std::endl;
+                        _list.remove(std::make_tuple(std::get<0>(i), std::get<1>(i), std::get<2>(i)));
+                        break;
+                    }
                 }
             }
-            if (isExist == false) {
-                _list_client.push_back(std::make_pair(_remote_endpoint, _clientNb));
-                _clientNb++;
-                std::cout << "il existe pas" << std::endl;
-            }
-            _socket.async_send_to(boost::asio::buffer(*message), _remote_endpoint,
-            boost::bind(&udp_server::handle_send, this, message,
+        }
+
+        void start_receive()
+        {
+            _socket.async_receive_from(
+                boost::asio::buffer(_recv_buffer), _remote_endpoint,
+                boost::bind(&udp_server::handle_receive, this,
                 boost::asio::placeholders::error,
                 boost::asio::placeholders::bytes_transferred));
-
-        start_receive();
         }
-    }
 
-    void handle_send(boost::shared_ptr<std::string> /*message*/,
-        const boost::system::error_code& /*error*/,
-        std::size_t /*bytes_transferred*/)
-    {
-    }
+        void handle_receive(const boost::system::error_code& error,
+            std::size_t buff_size)
+        {
+            if (!error) {
+                boost::shared_ptr<std::string> message(
+                new std::string(make_daytime_string()));
 
-    udp::socket _socket;
-    udp::endpoint _remote_endpoint;
-    boost::array<char, 1024> _recv_buffer;
-    std::list<std::pair<udp::endpoint, int> > _list_client;
-    int _clientNb;
+                bool isExist = false;
+                std::string data(_recv_buffer.begin(), buff_size);
+                std::cout << "Message de " << _remote_endpoint << ": " << data << std::endl;
+                for (auto const& i : _list_client) {
+                    if (std::get<0>(i) == _remote_endpoint) {
+                        std::cout << "il existe deja" << std::endl;
+                        *(std::clock_t *)&std::get<2>(i) = std::clock();
+                        isExist = true;
+                        break;
+                    }
+                }
+                if (isExist == false) {
+                    std::clock_t c_start = std::clock();
+                    _list_client.push_back(std::make_tuple(_remote_endpoint, _clientNb, c_start));
+                    _clientNb++;
+                    std::cout << "il existe pas" << std::endl;
+                }
+                _socket.async_send_to(boost::asio::buffer(*message), _remote_endpoint,
+                boost::bind(&udp_server::handle_send, this, message,
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+
+                start_receive();
+            }
+        }
+
+        void handle_send(boost::shared_ptr<std::string> /*message*/,
+            const boost::system::error_code& /*error*/,
+            std::size_t /*bytes_transferred*/)
+        {
+        }
+
+        udp::socket _socket;
+        udp::endpoint _remote_endpoint;
+        boost::array<char, 1024> _recv_buffer;
+        std::list<std::tuple<udp::endpoint, int, std::clock_t> > _list_client;
+        std::thread _thread_check;
+        int _clientNb;
 };
 
 int main(int argc, char **argv)
